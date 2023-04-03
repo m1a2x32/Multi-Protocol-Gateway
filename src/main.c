3d469613f876c72a5be4531ab1c82eeea8a539a4
Marco <i449698@fhict.nl>
2023-04-03 13:52:31 +0200

    fixed wifi not working

--- src/main.c
+++ src/main.c
@@ -1,106 +1,94 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) 2022 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
+
+/** @file
+ * @brief WiFi station sample
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(sta, CONFIG_LOG_DEFAULT_LEVEL);
+
+#include <nrfx_clock.h>
+#include <zephyr/kernel.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/init.h>
 
-#include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-
-#include <zephyr/net/net_core.h>
 #include <zephyr/net/net_if.h>
-#include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
 
-#define MYSSID "myssid"
-#define MYPSK "mypsk"
+#include <zephyr/net/socket.h>
 
-LOG_MODULE_REGISTER(mainThread, CONFIG_LOG_DEFAULT_LEVEL);
+#include <qspi_if.h>
 
-#define WIFI_MGMT_EVENTS ( 	NET_EVENT_WIFI_SCAN_RESULT | 		\
-							NET_EVENT_WIFI_SCAN_DONE  | 		\
-							NET_EVENT_WIFI_CONNECT_RESULT |		\
-							NET_EVENT_WIFI_DISCONNECT_RESULT)
+#include "net_private.h"
 
+#define WIFI_SHELL_MODULE "wifi"
 
-static uint32_t scan_result;
-static struct net_mgmt_event_callback wifi_sta_mgmt_cb;
-struct wifi_connect_req_params cnx_params;
+#define WIFI_SHELL_MGMT_EVENTS (NET_EVENT_WIFI_CONNECT_RESULT |		\
+				NET_EVENT_WIFI_DISCONNECT_RESULT)
 
-static int wifi_connect()
+#define MAX_SSID_LEN        32
+#define DHCP_TIMEOUT        70
+#define CONNECTION_TIMEOUT  100
+#define STATUS_POLLING_MS   300
+
+static struct net_mgmt_event_callback wifi_shell_mgmt_cb;
+static struct net_mgmt_event_callback net_shell_mgmt_cb;
+
+static struct {
+	const struct shell *sh;
+	union {
+		struct {
+			uint8_t connected	: 1;
+			uint8_t connect_result	: 1;
+			uint8_t disconnect_requested	: 1;
+			uint8_t _unused		: 5;
+		};
+		uint8_t all;
+	};
+} context;
+
+static int cmd_wifi_status(void)
 {
 	struct net_if *iface = net_if_get_default();
+	struct wifi_iface_status status = { 0 };
 
-	if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &cnx_params, sizeof(struct wifi_connect_req_params))) {
-		LOG_ERR("Error connecting to wifi station");
+	if (net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status,
+				sizeof(struct wifi_iface_status))) {
+		LOG_INF("Status request failed");
+
 		return -ENOEXEC;
 	}
 
-	LOG_INF("Initiating connection\n");
+	LOG_INF("==================");
+	LOG_INF("State: %s", wifi_state_txt(status.state));
+
+	if (status.state >= WIFI_STATE_ASSOCIATED) {
+		uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
+
+		LOG_INF("Interface Mode: %s",
+		       wifi_mode_txt(status.iface_mode));
+		LOG_INF("Link Mode: %s",
+		       wifi_link_mode_txt(status.link_mode));
+		LOG_INF("SSID: %-32s", status.ssid);
+		LOG_INF("BSSID: %s",
+		       net_sprint_ll_addr_buf(
+				status.bssid, WIFI_MAC_ADDR_LEN,
+				mac_string_buf, sizeof(mac_string_buf)));
+		LOG_INF("Band: %s", wifi_band_txt(status.band));
+		LOG_INF("Channel: %d", status.channel);
+		LOG_INF("Security: %s", wifi_security_txt(status.security));
+		LOG_INF("MFP: %s", wifi_mfp_txt(status.mfp));
+		LOG_INF("RSSI: %d", status.rssi);
+	}
 	return 0;
-}
-
-static int wifi_scan()
-{
-	struct net_if *iface = net_if_get_default();
-
-	if (net_mgmt(NET_REQUEST_WIFI_SCAN, iface, NULL, 0)) {
-		LOG_ERR("Scan request failed");
-		return -ENOEXEC;
-	}
-
-	LOG_INF("Scan Requested: \n");
-	return 0;
-}
-
-static void handle_wifi_scan_result(struct net_mgmt_event_callback *cb)
-{
-	const struct wifi_scan_result *entry =
-		(const struct wifi_scan_result *)cb->info;
-	uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
-	scan_result++;
-
-	if (scan_result == 1U) {
-		printf("\nNum | SSID | Len | Band | RSSI | Security\n");
-	}
-
-	printf("%-4d | %-32s %-5u | %-4u (%-6s) | %-4d | %-15s\n",
-	      scan_result, entry->ssid, entry->ssid_length, entry->channel,
-	      wifi_band_txt(entry->band),
-	      entry->rssi,
-	      wifi_security_txt(entry->security));	
-
-	if (strcmp(MYSSID, entry->ssid) == 0)
-	{
-		cnx_params.timeout = SYS_FOREVER_MS;
-		cnx_params.ssid = entry->ssid; // setup kconf file
-		cnx_params.ssid_length = strlen(MYSSID);
-		cnx_params.channel = entry->channel;
-		cnx_params.security = entry->security;
-		if (cnx_params.security!=WIFI_SECURITY_TYPE_NONE)
-		{
-			cnx_params.psk = MYPSK;
-			cnx_params.psk_length = strlen(MYPSK);
-		}
-		cnx_params.mfp = entry->mfp;
-	}
-}
-
-static void handle_wifi_scan_done(struct net_mgmt_event_callback *cb)
-{
-	const struct wifi_status *status =
-		(const struct wifi_status *)cb->info;
-
-	if (status->status) {
-		printf("Scan request failed\n");
-	} else {
-		printf("Scan request done\n Connecting to %s\n", MYSSID);
-		wifi_connect();
-	}
-	scan_result = 0U;
 }
 
 static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
@@ -108,11 +96,18 @@ static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
 	const struct wifi_status *status =
 		(const struct wifi_status *) cb->info;
 
+	if (context.connected) {
+		return;
+	}
+
 	if (status->status) {
-		LOG_ERR("Connection request failed (%d)", status->status);
+		LOG_ERR("Connection failed (%d)", status->status);
 	} else {
 		LOG_INF("Connected");
+		context.connected = true;
 	}
+
+	context.connect_result = true;
 }
 
 static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
@@ -120,20 +115,27 @@ static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
 	const struct wifi_status *status =
 		(const struct wifi_status *) cb->info;
 
-	LOG_ERR("Disconnected (%d)\n Reconnecting to %s", status->status, MYSSID);
-	wifi_connect();
+	if (!context.connected) {
+		return;
+	}
+
+	if (context.disconnect_requested) {
+		LOG_INF("Disconnection request %s (%d)",
+			 status->status ? "failed" : "done",
+					status->status);
+		context.disconnect_requested = false;
+	} else {
+		LOG_INF("Received Disconnected");
+		context.connected = false;
+	}
+
+	cmd_wifi_status();
 }
 
 static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
-				    uint32_t mgmt_event, struct net_if *iface)
+				     uint32_t mgmt_event, struct net_if *iface)
 {
 	switch (mgmt_event) {
-	case NET_EVENT_WIFI_SCAN_RESULT:
-		handle_wifi_scan_result(cb);
-		break;
-	case NET_EVENT_WIFI_SCAN_DONE:
-		handle_wifi_scan_done(cb);
-		break;
 	case NET_EVENT_WIFI_CONNECT_RESULT:
 		handle_wifi_connect_result(cb);
 		break;
@@ -145,12 +147,170 @@ static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
 	}
 }
 
+static void print_dhcp_ip(struct net_mgmt_event_callback *cb)
+{
+	/* Get DHCP info from struct net_if_dhcpv4 and print */
+	const struct net_if_dhcpv4 *dhcpv4 = cb->info;
+	const struct in_addr *addr = &dhcpv4->requested_ip;
+	char dhcp_info[128];
+
+	net_addr_ntop(AF_INET, addr, dhcp_info, sizeof(dhcp_info));
+
+	LOG_INF("DHCP IP address: %s", dhcp_info);
+}
+static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				    uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_IPV4_DHCP_BOUND:
+		print_dhcp_ip(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+static int __wifi_args_to_params(struct wifi_connect_req_params *params)
+{
+	params->timeout = SYS_FOREVER_MS;
+
+	/* SSID */
+	params->ssid = CONFIG_STA_SAMPLE_SSID;
+	params->ssid_length = strlen(params->ssid);
+
+#if defined(CONFIG_STA_KEY_MGMT_WPA2)
+	params->security = 1;
+#elif defined(CONFIG_STA_KEY_MGMT_WPA2_256)
+	params->security = 2;
+#elif defined(CONFIG_STA_KEY_MGMT_WPA3)
+	params->security = 3;
+#else
+	params->security = 0;
+#endif
+
+#if !defined(CONFIG_STA_KEY_MGMT_NONE)
+	params->psk = CONFIG_STA_SAMPLE_PASSWORD;
+	params->psk_length = strlen(params->psk);
+#endif
+	params->channel = WIFI_CHANNEL_ANY;
+
+	/* MFP (optional) */
+	params->mfp = WIFI_MFP_OPTIONAL;
+
+	return 0;
+}
+
+static int wifi_connect(void)
+{
+	struct net_if *iface = net_if_get_default();
+	static struct wifi_connect_req_params cnx_params;
+
+	context.connected = false;
+	context.connect_result = false;
+	__wifi_args_to_params(&cnx_params);
+
+	if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+		     &cnx_params, sizeof(struct wifi_connect_req_params))) {
+		LOG_ERR("Connection request failed");
+
+		return -ENOEXEC;
+	}
+
+	LOG_INF("Connection requested");
+
+	return 0;
+}
+
+int bytes_from_str(const char *str, uint8_t *bytes, size_t bytes_len)
+{
+	size_t i;
+	char byte_str[3];
+
+	if (strlen(str) != bytes_len * 2) {
+		LOG_ERR("Invalid string length: %zu (expected: %d)\n",
+			strlen(str), bytes_len * 2);
+		return -EINVAL;
+	}
+
+	for (i = 0; i < bytes_len; i++) {
+		memcpy(byte_str, str + i * 2, 2);
+		byte_str[2] = '\0';
+		bytes[i] = strtol(byte_str, NULL, 16);
+	}
+
+	return 0;
+}
+
 void main(void)
 {
-	net_mgmt_init_event_callback(&wifi_sta_mgmt_cb,
-				wifi_mgmt_event_handler,
-				WIFI_MGMT_EVENTS);
-	net_mgmt_add_event_callback(&wifi_sta_mgmt_cb);
+	int i;
+	memset(&context, 0, sizeof(context));
 
-	wifi_scan();
+	net_mgmt_init_event_callback(&wifi_shell_mgmt_cb,
+				     wifi_mgmt_event_handler,
+				     WIFI_SHELL_MGMT_EVENTS);
+
+	net_mgmt_add_event_callback(&wifi_shell_mgmt_cb);
+
+
+	net_mgmt_init_event_callback(&net_shell_mgmt_cb,
+				     net_mgmt_event_handler,
+				     NET_EVENT_IPV4_DHCP_BOUND);
+
+	net_mgmt_add_event_callback(&net_shell_mgmt_cb);
+
+	LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock/MHZ(1));
+	k_sleep(K_SECONDS(1));
+
+	if (strlen(CONFIG_NRF700X_QSPI_ENCRYPTION_KEY)) {
+		char key[QSPI_KEY_LEN_BYTES];
+		int ret;
+
+		ret = bytes_from_str(CONFIG_NRF700X_QSPI_ENCRYPTION_KEY, key, sizeof(key));
+		if (ret) {
+			LOG_ERR("Failed to parse encryption key: %d\n", ret);
+			return;
+		}
+
+		LOG_INF("QSPI Encryption enabled");
+
+		LOG_DBG("QSPI Encryption key: ");
+		for (int i = 0; i < QSPI_KEY_LEN_BYTES; i++) {
+			LOG_DBG("%02x", key[i]);
+		}
+		LOG_DBG("\n");
+
+		qspi_configure_encryption(key);
+		qspi_enable_encryption();
+	} else {
+		LOG_INF("QSPI Encryption disabled");
+	}
+
+	LOG_INF("Static IP address (overridable): %s/%s -> %s",
+		CONFIG_NET_CONFIG_MY_IPV4_ADDR,
+		CONFIG_NET_CONFIG_MY_IPV4_NETMASK,
+		CONFIG_NET_CONFIG_MY_IPV4_GW);
+
+	while (1) {
+		wifi_connect();
+
+		for (i = 0; i < CONNECTION_TIMEOUT; i++) {
+			k_sleep(K_MSEC(STATUS_POLLING_MS));
+			cmd_wifi_status();
+			if (context.connect_result) {
+				break;
+			}
+		}
+		if (context.connected) {
+			int err = zsock_socket(AF_INET,SOCK_STREAM,IPPROTO_IP);
+			if (err)
+			{
+				LOG_ERR("Socket creation error = %d", err);
+			}
+			
+			k_sleep(K_FOREVER);
+		} else if (!context.connect_result) {
+			LOG_ERR("Connection Timed Out");
+		}
+	}
 }
